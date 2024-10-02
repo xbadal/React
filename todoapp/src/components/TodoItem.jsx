0fe1ad602142cdab4f74a79449866f4a3a3d3f05
Badal kumar <kbadal001@gmail.com>
2024-10-02 11:17:40 +0530

    Delete Items

--- todoapp/src/components/TodoItem.jsx
+++ todoapp/src/components/TodoItem.jsx
@@ -1,9 +1,24 @@
 import styles from "./todoitem.module.css";
 
-export default function TodoItem(props) {
+export default function TodoItem({ item, todos, setTodos }) {
+  function handleClick(item) {
+    setTodos(todos.filter((todo) => todo != item));
+  }
+
   return (
     <div className={styles.item}>
-      <div className={styles.itemName}>{props.item}</div>
+      <div className={styles.itemName}>
+        {item}{" "}
+        <span>
+          <button
+            className={styles.deleteButton}
+            onClick={() => handleClick(item)}
+          >
+            x
+          </button>
+        </span>
+      </div>
+
       <hr className={styles.line} />
     </div>
   );
